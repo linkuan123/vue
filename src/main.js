dfe83296de6e392400558fc89e9c0ab1c7ca56ea
K <45218419+linkuan123@users.noreply.github.com>
2020-01-13 23:33:55 +0800

    login function finish

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import './plugins/iview.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
+// 用于发送ajax
+import axios from 'axios'
+// 配置请求的跟路径
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+new Vue({ router, render: h => h(App) }).$mount('#app')
